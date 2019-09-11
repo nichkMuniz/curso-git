a3f76e4de1a4424b0939c7ecc78f70e44c730c75
Nicholas <nicholasmuniz19@gmail.com>
2019-09-10 23:29:44 -0300

    Adicionado a frase de bom dia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como vai voce?");
+            Console.WriteLine("Espero que voce tenha um otimo dia! :) ");
             Console.ReadLine();
         }
     }
